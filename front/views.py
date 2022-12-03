003d4a8848fcb52542de61276351a7ef0eddac13
Kraken <burgard.thomas@rocketmail.com>
2022-12-03 13:37:48 +0100

    fix and begin route

--- front/views.py
+++ front/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
+from front import Process_data_ifre
+
+
 # Create your views here.
 def home(request):
-    context = {}
+    return_dict = Process_data_ifre.get_total_jobs()
+    context = {
+        
+    }
     return render(request, "template", context)
+
+# Wishlist
+# les métier liké
+
+# Coup de coeur
+# retour algo
+# liste des métiers + infos
+# listes des formations correspondantes
+
+# une route pour afficher ele premier onboarding
+
+# une route pour l'api des info
